ce4b355b0065f3a8403a2cafbe9baf34883279e3
Moses Kamara <kamara.moses@yahoo.com>
2020-10-19 18:39:21 -0700

    finished coding the app

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 //use static files
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fittrack', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 //require('./seeders/seed')
 

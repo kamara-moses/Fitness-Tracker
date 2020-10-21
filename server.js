9295d8c11bffef6d622d868ef443a70e1fbfc0cd
Moses Kamara <kamara.moses@yahoo.com>
2020-10-20 17:49:01 -0700

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 //use static files
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker', { useNewUrlParser: true });
 
 require("./routes/api-routes.js")(app);
 
